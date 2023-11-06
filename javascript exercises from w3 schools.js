cc4d001547eaecd79660c42ec70a27f090b3fcdf
Christine Hoang <christinehoangt@gmail.com>
2023-11-05 20:57:59 -0600

    initcommit

--- javascript exercises from w3 schools.js
+++ javascript exercises from w3 schools.js
@@ -1,12 +1,18 @@
 let carName = "Volvo";
 console.log(carName);
 
+////////////////////////////////////////////
+
 let x = 50
 console.log(x);
+
+////////////////////////////////////////////
 
 let b = 5;
 let c = 10;
 document.getElementById("demo").innerHTML = b + c;
+
+////////////////////////////////////////////
 
 let a = 5;
 let y = 10;
@@ -14,8 +20,12 @@ let z = a + y;
 alert(z);
 console.log(z);
 
+////////////////////////////////////////////
+
 let firstName = "John", lastName = "Doe", age = 35;
 console.log(firstName);
+
+////////////////////////////////////////////
 
 alert(10 * 5);
 
@@ -23,6 +33,27 @@ alert(10 / 2);
 
 alert(15 % 9);
 
+////////////////////////////////////////////
+//choose the correct operator
+
 x = 10;
 y = 5;
 x += y;
+
+////////////////////////////////////////////
+//choose the correct operator
+
+x = 10;
+y = 5;
+x *= y;
+
+////////////////////////////////////////////
+
+let length = 16;          // number
+let lastName = "Johnson"; //string
+const x = {
+  firstName: "John",
+  lastName: "Doe"
+};                        //object
+
+
